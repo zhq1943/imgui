de0f78d645510bbb47385b7266e0f18a0e068184
Ben Carter <ben@shironekolabs.com>
2019-12-05 15:48:41 +0900

    Texture-based round corners: Added support for multiple stroke widths

--- imgui_internal.h
+++ imgui_internal.h
@@ -2043,8 +2043,15 @@ namespace ImGui
 
 } // namespace ImGui
 
-// ImFontAtlas internals
+// Note that stroke width increases effective radius, so (e.g.) a max radius circle will have to use the fallback path if stroke width is > 1
 const unsigned int          ImFontAtlasRoundCornersMaxSize = 32; // Maximum size of rounded corner texture to generate in fonts
+const unsigned int          ImFontAtlasRoundCornersMaxStrokeWidth = 5; // Maximum stroke width of rounded corner texture to generate in fonts
+// Bit mask for which stroke widths should have textures generated for them (the default of 0xD means widths 1, 2 and 4)
+// Only bits up to ImFontAtlasRoundCornersMaxStrokeWidth are considered, and bit 0 (stroke width 1) must always be set
+// Optimally there should be an odd number of bits set, as the texture packing packs the data in pairs, with one half of one pair being occupied by the filled texture
+const unsigned int          ImFontAtlasRoundCornersStrokeWidthMask = 0xD;
+
+// ImFontAtlas internals
 IMGUI_API bool              ImFontAtlasBuildWithStbTruetype(ImFontAtlas* atlas);
 IMGUI_API void              ImFontAtlasBuildInit(ImFontAtlas* atlas);
 IMGUI_API void              ImFontAtlasBuildSetupFont(ImFontAtlas* atlas, ImFont* font, ImFontConfig* font_config, float ascent, float descent);
